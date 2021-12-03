87e014a4e92114b30d3fbed29b2366bb070e597d
Greg Munro <gregmunro90@gmail.com>
2021-12-03 18:15:38 +0100

    wip it 6

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -5,6 +5,34 @@ export default function NewBeer() {
   return (
     <div>
       <Header />
+      <form>
+        <label>Name </label>
+        <input type="text" name="name" value={name} />
+
+        <label>Tagline</label>
+        <input type="text" name="tagline" value={tagline} />
+
+        <label>Description </label>
+        <input type="text" name="description" value={description} />
+
+        <label>First Brewed </label>
+        <input type="text" name="first brewed" checked={first_brewed} />
+
+        <label>Brewers Tips </label>
+        <input type="text" name="brewers tips" checked={brewers_tips} />
+
+        <label>Attenuation Level </label>
+        <input
+          type="number"
+          name="Attenuation level"
+          checked={attenuation_level}
+        />
+
+        <label>Contributed By </label>
+        <input type="text" name="Contributed by" checked={contributed_by} />
+
+        <button type="submit">Add a Movie</button>
+      </form>
     </div>
   );
 }
--- src/pages/RandomBeer.jsx
+++ src/pages/RandomBeer.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import Header from '../components/Header';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function RandomBeer() {
+import axios from 'axios';
+
+export default function AllBeers() {
+  const [beers, setBeers] = useState({});
+  const { id: beerId } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://ih-beers-api2.herokuapp.com/beers/random`)
+      .then((response) => {
+        console.log('beers date', response.data);
+        setBeers(response.data);
+      })
+
+      .catch(console.log);
+  }, []);
+
   return (
     <div>
       <Header />
+      <br />
+
+      <img src={beers.image_url} width="60px" alt="beer" />
+      <h3>{beers.name}</h3>
+      <h5>{beers.tagline}</h5>
+      <p>First brewed: {beers.first_brewed}</p>
+      <p>Attenuation level: {beers.attenuation_level}</p>
+      <p>{beers.description}</p>
+      <p>Contributed by: {beers.contributed_by}</p>
     </div>
   );
 }
