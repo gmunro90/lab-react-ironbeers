43f321748546996e97932c2b0b4aeec14b368ae5
Greg Munro <gregmunro90@gmail.com>
2022-01-26 11:49:46 +0100

    fixed compile

--- src/pages/NewBeer.jsx
+++ src/pages/NewBeer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Switch, Route } from 'react-router';
 
 export default function NewBeer() {
   return (
@@ -7,29 +8,29 @@ export default function NewBeer() {
       <Header />
       <form>
         <label>Name </label>
-        <input type="text" name="name" value={name} />
+        <input type="text" name="name" value={'name'} />
 
         <label>Tagline</label>
-        <input type="text" name="tagline" value={tagline} />
+        <input type="text" name="tagline" value={'tagline'} />
 
         <label>Description </label>
-        <input type="text" name="description" value={description} />
+        <input type="text" name="description" value={'description'} />
 
         <label>First Brewed </label>
-        <input type="text" name="first brewed" checked={first_brewed} />
+        <input type="text" name="first brewed" checked={'first_brewed'} />
 
         <label>Brewers Tips </label>
-        <input type="text" name="brewers tips" checked={brewers_tips} />
+        <input type="text" name="brewers tips" checked={'brewers_tips'} />
 
         <label>Attenuation Level </label>
         <input
           type="number"
           name="Attenuation level"
-          checked={attenuation_level}
+          checked={'attenuation_level'}
         />
 
         <label>Contributed By </label>
-        <input type="text" name="Contributed by" checked={contributed_by} />
+        <input type="text" name="Contributed by" checked={'contributed_by'} />
 
         <button type="submit">Add a Movie</button>
       </form>
